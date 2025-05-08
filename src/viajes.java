0b12152b54909d57781cf643fcf721e9a4163d63
luarango <luisa.arango131@gmail.com>
2025-05-08 18:15:31 -0500

    version

---
+++ src/viajes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; //Ejercicio 19//
+public class viajes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+        System.out.print("¿Tiene pasaporte? (1 para sí, 0 para no): ");
+        int Pasaporte = sc.nextInt();
+        if (edad > 18 && Pasaporte == 1) {
+            System.out.println("Puede viajar");
+        } else {
+            System.out.println("No puede viajar");
+        }
+    }
+}
+
